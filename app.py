248a752b3c120f2f75d7793788650229f52ad0f9
HussainMotiwala <motiwalahussain52@gmail.com>
2025-07-11 14:50:59 +0530

    Main
    Main

---
+++ app.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pickle
+import streamlit as st
+
+model = pickle.load(open("Social_Network_Ads.pkl","rb"))
+
+st.title("Social Network Ads Prediction")
+
+age = st.number_input("Enter Age","18")
+salary = st.number_input("Enter Estimated Salary","10000")
+
+features = np.array([[age, salary]])
+
+group = model.predict(features)
+
+st.write(f"Hello, *World!* :sunglasses: . Customer Group {group}")
+
